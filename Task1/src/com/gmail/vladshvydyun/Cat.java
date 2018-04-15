15ff11e45c3785b313812f403f95695b769037c4
Vladislav <vladshvydyun@gmail.com>
2018-04-15 15:54:40 +0300

    Add class Cat and give it the properties and methods.

---
+++ Task1/src/com/gmail/vladshvydyun/Cat.java
@@ -0,0 +1,63 @@
+/* Описать класс «Cat» (в качестве образца можно взять домашнего питомца). 
+ * Наделить его свойствами и методами. Создать несколько экземпляров объектов
+ * этого класса. Использовать эти объекты.
+ * */
+
+package com.gmail.vladshvydyun;
+
+public class Cat {
+	private String name;
+	private int age;
+	private int weight;
+	private int runSpeed;
+
+	public Cat() {
+		super();
+	}
+
+	public Cat(String name, int age, int weight) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public void makeSound() {
+		System.out.println("Maaay-maaay");
+	}
+
+	public void runSpeed(int speed) {
+		runSpeed += runSpeed + speed;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [name=" + name + ", age=" + age + ", weight=" + weight + "kg" + ", runSpeed=" + runSpeed + "km/h"
+				+ "]";
+	}
+
+}
--- Task1/src/com/gmail/vladshvydyun/Main.java
+++ Task1/src/com/gmail/vladshvydyun/Main.java
@@ -3,7 +3,13 @@ package com.gmail.vladshvydyun;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Cat catOne = new Cat("Barsik", 12, 23);
+		catOne.runSpeed(5);
+		catOne.makeSound();
+		catOne.setAge(14);
+
+		System.out.println(catOne.toString());
 
 	}
 
