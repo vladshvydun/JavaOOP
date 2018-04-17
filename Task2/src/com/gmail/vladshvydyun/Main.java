b56ee878d26a0817e70aaee96fa00266322aa7d1
Vladislav <vladshvydyun@gmail.com>
2018-04-17 21:05:51 +0300

    Added get and set methods

--- Task2/src/com/gmail/vladshvydyun/Main.java
+++ Task2/src/com/gmail/vladshvydyun/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
 		Triangle triangleOne = new Triangle(3.4, 5, 7.5);
 		Triangle triangleTwo = new Triangle(6, 3.3, 7);
-		
+
 		System.out.println("Area of triangleOne: " + triangleOne.triangleArea());
 		System.out.println("Area of triangleTwo: " + triangleTwo.triangleArea());
 
 	}
-
 }
--- Task2/src/com/gmail/vladshvydyun/Triangle.java
+++ Task2/src/com/gmail/vladshvydyun/Triangle.java
@@ -16,6 +16,30 @@ public class Triangle {
 		this.sideThree = sideThree;
 	}
 
+	public double getSideOne() {
+		return sideOne;
+	}
+
+	public void setSideOne(double sideOne) {
+		this.sideOne = sideOne;
+	}
+
+	public double getSideTwo() {
+		return sideTwo;
+	}
+
+	public void setSideTwo(double sideTwo) {
+		this.sideTwo = sideTwo;
+	}
+
+	public double getSideThree() {
+		return sideThree;
+	}
+
+	public void setSideThree(double sideThree) {
+		this.sideThree = sideThree;
+	}
+
 	public double triangleArea() {
 		double p = (sideOne + sideTwo + sideThree) / 2;
 		return Math.sqrt((p * (p - sideOne) * (p - sideTwo) * (p - sideThree)));
