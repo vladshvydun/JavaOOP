55f27c7d09e4a9ae1ed3d57ff902346d742a1525
vladshvydun <vladshvydyun@gmail.com>
2018-05-14 23:37:48 +0300

    Task: Create ArrayList and del elements from head and tail

---
+++ Task6a/src/com/gmail/vladshvydyun/Main.java
@@ -0,0 +1,32 @@
+package com.gmail.vladshvydyun;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        myList();
+
+    }
+
+    public static void myList() {
+        ArrayList<Integer> myList = new ArrayList<>();
+        System.out.println("List created");
+
+        Random rn = new Random();
+        for (int i = 0; i < 10; i++) {
+            myList.add(rn.nextInt(10));
+        }
+        System.out.println("Added elements");
+        System.out.println(myList);
+
+        myList.remove(0);
+        myList.remove(0);
+        myList.remove(myList.size() - 1);
+
+        System.out.println("Deleted first two elements and last: ");
+        System.out.println(myList);
+    }
+
+}
